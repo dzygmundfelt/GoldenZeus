db26f4abc5ce63e5b95ca453b38f749c3b92c0fc
Dan Zygmund-Felt <d.zygmund.felt@gmail.com>
2017-07-23 19:19:40 -0400

    cleaned up some print statements, added a few comments

--- MLAlgorithm.py
+++ MLAlgorithm.py
@@ -1,4 +1,3 @@
-import MLDataFormatter
 import PoloniexDataFetcher as pdf
 import requests
 import json
@@ -24,7 +23,6 @@ fd, td = pdf.getDateRangeFromUser()
 from_date, to_date = pdf.convertRangeToUTC(fd, td)
 period = str(raw_input("Choose 300, 900, 1800, 7200, 14400, 86400 second time periods.\n"))
 url = pdf.createPoloniexURL(from_date, to_date, period)
-print(url)
 data = pdf.getData(url)
 ml_array = putDataIntoArray(data)
 
@@ -32,14 +30,15 @@ normal_dates = normalizeArray(ml_array[0])
 normal_volumes = normalizeArray(ml_array[1])
 weightedAverages = normalizeArray(ml_array[2])
 
-# next_utc = normal_dates.pop()
-# next_volume = normal_volumes.pop()
+next_utc = normal_dates.pop()
+next_volume = normal_volumes.pop()
 weightedAverages = weightedAverages[1:]
 vector_length = len(normal_dates)
 
 for i in range(vector_length):
 	print("date: " + str(normal_dates[i]) + " volume: " + str(normal_volumes[i]) + " next period avg: " + str(weightedAverages[i]))
 
+# set up computational graph
 W1 = tf.Variable(0.3, tf.float32)
 W2 = tf.Variable(0.3, tf.float32)
 b = tf.Variable(0.1, tf.float32)
@@ -48,8 +47,11 @@ x2 = tf.placeholder(tf.float32)
 y = tf.placeholder(tf.float32)
 linear_model = W1 * x1 + W2 * x2 + b
 
+# define loss function
 squared_deltas = tf.square((linear_model - y) / (2*len(normal_volumes)))
 loss = tf.reduce_sum(squared_deltas)
+
+# create session and run it
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
@@ -68,6 +70,6 @@ for i in range(1000):
 
 print(sess.run([W1, W2, b]))
 
-# print(str(next_utc) + ' ' + str(next_volume) + ' ' + str(weightedAverages[]))
+print(str(next_utc) + ' ' + str(next_volume) + ' ' + str(weightedAverages[-1]))
 
-# print(next_utc*W1_val + next_volume*W2_val + b_val - weightedAverages[-1])
+print(next_utc*W1_val + next_volume*W2_val + b_val - weightedAverages[-1])
